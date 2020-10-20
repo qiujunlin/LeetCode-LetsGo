a37395cb330724465d25490f6ba363f4f811ad4e
qiujunlin <1757591067@qq.com>
2020-10-20 21:46:34 +0800

    SumRoottoLeafNumbers129

---
+++ src/LinkedList/SwapNodesinPairs.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+
+
+ class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+public class SwapNodesinPairs {
+    public ListNode swapPairs(ListNode head) {
+  return  null;
+    }
+}
---
+++ src/Tree/SymmetricTree.java
@@ -0,0 +1,14 @@
+package Tree;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        return compare(root.left,root.right);
+    }
+    boolean compare(TreeNode node1,TreeNode node2){
+        if(node1==null&&node2==null) return true;
+        if(node1==null||node2==null) return false;
+        return (node1.val==node2.val)&&compare(node1.left,node2.right)&&compare(node1.right,node2.left);
+
+    }
+}
