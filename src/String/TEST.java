c159aa747338def3f6cbe0f693ba195604c08440
qiujunlin <1757591067@qq.com>
2020-10-28 09:58:39 +0800

    HowManyNumbersAreSmallerThantheCurrentNumber1365

--- src/String/TEST.java
+++ src/String/TEST.java
@@ -1,18 +1,21 @@
 package String;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class TEST {
     public static void main(String[] args) {
-        List<Integer> list= new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        Collections.reverse(list);
-        System.out.println(list);
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(1,2);
+        map.put(1,3);
+        Set<Integer> set= new HashSet<>();
+        for (Integer a:map.keySet()) {
+            if(set.contains(a)){
+                return;
+            }else{
+                set.add(map.get(a));
+            }
+        }
+        System.out.println(map.get(1));
     }
 
 }
