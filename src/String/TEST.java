9643c1c9093603aef1034c75aa8e14f52b5ef589
qiujunlin <1757591067@qq.com>
2020-11-02 13:02:18 +0800

    343

--- src/String/TEST.java
+++ src/String/TEST.java
@@ -6,7 +6,7 @@ public class TEST {
     public static void main(String[] args) {
          Queue<Integer> queue = new LinkedList<>();
 
-       // System.out.println(map.get(1));
+
         Map<Integer,Integer> map = new HashMap<>();
         map.put(1,map.getOrDefault(1,0)+1);
         List<Integer> i= new ArrayList<>();
