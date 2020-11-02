584ef8de3ca939857b1ce4e599a24a987f7ddd95
qiujunlin <1757591067@qq.com>
2020-11-02 11:08:43 +0800

    343

---
+++ src/Array/IntersectionofTwoArrays349.java
@@ -0,0 +1,44 @@
+package Array;
+
+import java.util.*;
+
+/**
+ * @Classname IntersectionofTwoArrays349
+ * @Description:
+ * @Date 2020/11/2 11:08
+ * @Created by qiujunlin
+ */
+public class IntersectionofTwoArrays349 {
+    //使用双指针
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        Set<Integer> set = new HashSet<>();
+        int i=0,j=0;
+        while(i<nums1.length&&j<nums2.length){
+            if(nums1[i]<nums2[j]){
+                i++;
+                continue;
+            }else if(nums1[i]>nums2[j]){
+                j++;
+                continue;
+            }else if(nums1[i]==nums2[j]){
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+
+
+        }
+
+        int a[]= new int[set.size()];
+        int k=0;
+        for(Integer c:set){
+            a[k++]=c;
+        }
+
+        return a;
+
+
+    }
+}
--- src/String/TEST.java
+++ src/String/TEST.java
@@ -7,6 +7,14 @@ public class TEST {
          Queue<Integer> queue = new LinkedList<>();
         // queue.
        // System.out.println(map.get(1));
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(1,map.getOrDefault(1,0)+1);
+        List<Integer> i= new ArrayList<>();
+        i.toArray();
+        for (Integer integer:
+            map.keySet() ) {
+
+        }
     }
 
 }
