149e43ac37dfde934014e30e65df44bb9aa654ac
qiujunlin <1757591067@qq.com>
2020-11-03 17:14:01 +0800

    PopulatingNextRightPointersinEachNodeII117

---
+++ src/Tree/PopulatingNextRightPointersinEachNodeII117.java
@@ -0,0 +1,33 @@
+package Tree;
+
+import java.util.*;
+
+/**
+ * @Description:
+ * @Author: qiujunlin
+ * @Date: 2020/11/3
+ */
+public class PopulatingNextRightPointersinEachNodeII117 {
+    public Node connect(Node root) {
+        if(root==null) return null;
+        Queue<Node> queue= new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int num = queue.size();
+            for(int i=1;i<=num;i++){
+                Node node=queue.poll();
+                if(i==num){
+                    node.next=null;
+
+                }else{
+                    node.next=  queue.element();
+                }
+
+                if(node.left!=null) queue.offer(node.left);
+                if(node.right!=null) queue.offer(node.right);
+            }
+        }
+        return root;
+
+    }
+}
