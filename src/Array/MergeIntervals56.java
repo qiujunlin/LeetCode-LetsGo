eb1b7ed3499571b2631829b1ea24732c12017c66
qiujunlin <1757591067@qq.com>
2020-11-04 18:20:09 +0800

    sametree

---
+++ src/Array/MergeIntervals56.java
@@ -0,0 +1,12 @@
+package Array;
+
+import java.util.*;
+
+/**
+ * @Classname MergeIntervals56
+ * @Description:
+ * @Date 2020/11/4 9:58
+ * @Created by qiujunlin
+ */
+public class MergeIntervals56 {
+}
