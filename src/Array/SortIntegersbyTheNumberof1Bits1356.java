c46b9df71c3270698ae0f47ccf1035111b600fb8
qiujunlin <1757591067@qq.com>
2020-11-06 12:38:09 +0800

    SortIntegersbyTheNumberof1Bits1356

--- src/Array/SortIntegersbyTheNumberof1Bits1356.java
+++ src/Array/SortIntegersbyTheNumberof1Bits1356.java
@@ -9,7 +9,7 @@ import java.util.*;
  * @Created by qiujunlin
  */
 public class SortIntegersbyTheNumberof1Bits1356 {
-    //第一次代码
+    //第一次代码  速度超过百分之5
     public int[] sortByBits(int[] arr) {
 
         Integer[] nums = new Integer[arr.length];
@@ -34,4 +34,38 @@ public class SortIntegersbyTheNumberof1Bits1356 {
         }
         return   res;
     }
+    //版本2 使用Integer.bitCount(a)方法后性能 超过了百分之50
+    public int[] sortByBits2(int[] arr) {
+        // Map<Integer,Integer> map = new HashMap<>();
+        // for(int i=0;i<arr.length;i++){
+        //   map.put(arr[i],cout(arr[i]));
+        // }
+        Integer[] nums = new Integer[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            nums[i] = arr[i];
+        }
+        Arrays.sort(nums,(Integer a,Integer b)->{
+            int res1=Integer.bitCount(a);//使用了移位操作 就很快
+            int res12=Integer.bitCount(b);
+            return res1==res12?a-b:res1-res12;
+        });
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = nums[i];
+        }
+        return arr;
+    }
+    //第三种 超级超级巧妙的方法 记住
+    public int[] sortByBits3(int[] arr) {
+        int[] map = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            map[i] = Integer.bitCount(arr[i]) * 10000000 + arr[i];
+        }
+        Arrays.sort(map);
+        for (int i = 0; i < map.length; i++) {
+            map[i] = map[i] % 10000000;
+        }
+        return map;
+    }
+
+
 }
