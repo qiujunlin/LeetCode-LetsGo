2358f2783d98f681d3df22a5d31dc1c95a1c7825
qiujunlin <1757591067@qq.com>
2020-11-08 15:26:20 +0800

    InvertBinaryTree226

--- src/String/TEST.java
+++ src/String/TEST.java
@@ -6,43 +6,11 @@ import java.util.*;
 
 public class TEST {
     public static void main(String[] args) {
-
-
-          List<Integer> list=new ArrayList<>();
-          list.add(1);
-          list.add(2);
-          list.add(3);
-          Integer[] a = list.toArray(new Integer[list.size()]);
-          System.out.println(a[2]);
-
-    }
-    public int[] sortByBits(Integer[] arr) {
-        // Map<Integer,Integer> map = new HashMap<>();
-        // for(int i=0;i<arr.length;i++){
-        //   map.put(arr[i],cout(arr[i]));
-        // }
-        Arrays.sort(arr,(Integer a,Integer b)->a-b);
-        return null;
-    }
-    public static  int cout(int n){
-        int res=0;
-        while(n!=0){
-            if(n%2==1) res++;
-            n = n/2;
+        int[] a = {4,6,8,2,1,4,9,2,4};
+        Arrays.sort(a);
+        for(int arr:a) {
+            System.out.print(arr + " ");
         }
-        return   res;
-    }
-    public static  boolean uniqueOccurrences(int[] arr) {
-        Map<Integer,Integer> map= new HashMap<>();
-
-        for (int a:arr) {
-            map.put(a ,map.getOrDefault(a, 0) + 1);
-        }
-        Set<Integer> set= new HashSet<>(map.values());
-        for(Integer a:map.values()){
-            if(set.add(a)==false) return false;
-        }
-        return true;
     }
 
 }
