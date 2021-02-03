633230dbf1bd922647dc1a5265480d41af0ab1a5
qiujunlin <1757591067@qq.com>
2021-02-04 00:25:19 +0800

    并查集
    huad
    
    l

--- docs/data-structure/linkedlist/linkedlist.md
+++ docs/data-structure/linkedlist/linkedlist.md
@@ -7,3 +7,41 @@
 | 1    | 两数之和 | hash   | easy   |
 | 15   | 三数之和 | 双指针 | middle |
 |      |          |        |        |
+
+## 2.两数相加
+
+思路：
+
+关键点： 
+
+* 如果为空  就用0 占位
+
+* 使用一个进位，每次都要加上上一次的进位。在最后一步的时候，还好需要判断进位是否为0  来加上最后一位。
+* 链表的解法中 一般都需要在第一位加上一个“哨兵” 作为  头结点
+
+```java
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int jinwei = 0;
+        ListNode cur = new ListNode(0);
+        ListNode res =cur ; 
+        while(l1!=null||l2!=null||jinwei!=0){
+          int num1 = 0;
+          int num2 = 0;
+    
+         num1 =  l1==null? 0 : l1.val;
+         num2 = l2==null?0 : l2.val;
+         int num =  num1 + num2 + jinwei;
+         jinwei = num / 10;
+         num = num % 10;
+         ListNode newlist = new ListNode(num);
+         cur.next = newlist;
+         cur  =  cur.next;
+         if(l1!=null)l1 = l1.next;
+         if(l2!=null)l2 = l2.next;
+        }
+        return res.next;
+    }
+}
+```
+
---
+++ src/Array/SlidingWindowMedian480.java
@@ -0,0 +1,105 @@
+package Array;
+
+import java.util.*;
+
+/**
+ * @Classname SlidingWindowMedian480
+ * @Description:
+ * @Date 2021/2/3 11:45
+ * @Created by qiujunlin
+ */
+
+class DualHeap {
+    // 大根堆，维护较小的一半元素
+    private PriorityQueue<Integer> small;
+    // 小根堆，维护较大的一半元素
+    private PriorityQueue<Integer> large;
+    // 哈希表，记录「延迟删除」的元素，key 为元素，value 为需要删除的次数
+    private Map<Integer, Integer> delayed;
+    private int k;
+    // small 和 large 当前包含的元素个数，需要扣除被「延迟删除」的元素
+    private int smallSize, largeSize;
+
+    public DualHeap(int k) {
+        this.small = new PriorityQueue<Integer>((o1, o2) -> o2 - o1);
+        this.large = new PriorityQueue<Integer>();
+        this.delayed = new HashMap<Integer, Integer>();
+        this.k = k;
+        this.smallSize = 0;
+        this.largeSize = 0;
+    }
+
+    public double getMedian() {
+        return (k & 1) == 1 ? small.peek() : ((double) small.peek() + large.peek()) / 2;
+    }
+
+    //在进行插入的时候，
+    public void insert(int num) {
+        if (small.isEmpty() || num <= small.peek()) {
+            smallSize++;
+            small.offer(num);
+        } else {
+            largeSize++;
+            large.offer(num);
+        }
+        makeBalance();
+
+    }
+
+    public void erase(int num) {
+        delayed.put(num, delayed.getOrDefault(num, 0) + 1);
+        if (num <= small.peek()) {
+            smallSize--;
+            if (num == small.peek()) {
+               prune(small);
+            }
+        } else {
+            largeSize--;
+            if (num == large.peek()) {
+                prune(large);
+            }
+        }
+        makeBalance();
+    }
+
+    // 不断地弹出 heap 的堆顶元素，并且更新哈希表
+    private void prune(PriorityQueue<Integer> heap) {
+        while (!heap.isEmpty() && delayed.containsKey(heap.peek())) {
+            int temp = heap.poll();
+            delayed.put(temp, delayed.get(temp) - 1);
+            if (delayed.get(temp) == 0) {
+                delayed.remove(temp);
+            }
+        }
+    }
+
+    // 调整 small 和 large 中的元素个数，使得二者的元素个数满足要求
+    private void makeBalance() {
+        if (smallSize - largeSize >= 2) {
+            int temp = small.poll();
+            smallSize--;
+            large.offer(temp);
+            largeSize++;
+            prune(small);
+        } else if (smallSize < largeSize) {
+            int temp = large.poll();
+            smallSize ++;
+            largeSize--;
+            large.offer(temp);
+            prune(large);
+        }
+
+    }
+}
+
+public class SlidingWindowMedian480 {
+    public static void main(String[] args) {
+        Collections.reverseOrder();
+    }
+
+    public double[] medianSlidingWindow(int[] nums, int k) {
+
+
+    }
+
+}
