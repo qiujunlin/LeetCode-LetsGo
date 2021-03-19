4029f4ecda210e2a238c53d0972c6bed70c5456c
qiujunlin <1757591067@qq.com>
2021-03-20 00:40:35 +0800

    1
    huad
    
    l

---
+++ src/Array/Q150.java
@@ -0,0 +1,43 @@
+package Array;
+
+import java.util.*;
+
+/**
+ * @Classname Q150
+ * @Description:
+ * @Date 2021/3/20 0:09
+ * @Created by qiujunlin
+ */
+public class    Q150 {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<tokens.length;i++){
+            if(i<=1) stack.push(Integer.valueOf(tokens[i]));
+            else{
+                String s  = tokens[i];
+                if(Character.isDigit(s.charAt(0))){
+                    stack.push(Integer.valueOf(s));
+                }else if(s.equals("+")){
+                    Integer num1 = stack.pop();
+                    Integer num2 = stack.pop();
+                    stack.push(num2+num1);
+                }else  if(s.equals("-")){
+                    Integer num1 = stack.pop();
+                    Integer num2 = stack.pop();
+                    stack.push(num2-num1);
+                }else if(s.equals("/")){
+                    Integer num1 = stack.pop();
+                    Integer num2 = stack.pop();
+                    stack.push(num2/num1);
+                }else  if(s.equals("*")){
+                    Integer num1 = stack.pop();
+                    Integer num2 = stack.pop();
+                    stack.push(num2*num1);
+                }
+            }
+            return  stack.peek();
+        }
+
+
+    }
+}
