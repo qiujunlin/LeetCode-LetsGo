3c8c9a345f214a6d8f609f94577cfc358beebb3f
qiujunlin <1757591067@qq.com>
2021-04-05 00:25:04 +0800

    算法4 代码

--- src/Array/Q5780.java
+++ src/Array/Q5780.java
@@ -9,4 +9,25 @@ import java.util.*;
  * @Created by qiujunlin
  */
 public class Q5780 {
+    int mod = (int) (1e9+7);
+    public int countNicePairs(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        for (int i = 0;i<nums.length;i++){
+            int sum = nums[i]-reverse(nums[i]);
+            res=(res+map.getOrDefault(sum,0))%mod;
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return  res;
+    }
+
+    int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
+        return res;
+    }
 }
+
