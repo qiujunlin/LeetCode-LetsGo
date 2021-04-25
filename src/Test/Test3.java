49f14f1dd53388ddbd3cee7ca22baa82fb4f0205
qiujunlin <1757591067@qq.com>
2021-04-25 22:12:13 +0800

    1

--- src/Test/Test3.java
+++ src/Test/Test3.java
@@ -1,6 +1,8 @@
 package Test;
 
 import java.util.*;
+
+import Al4.Graph.KruskalMST;
 import  edu.princeton.cs.algs4.*;
 
 /**
@@ -18,10 +20,22 @@ interface GreetingService
 public class Test3 {
 
     public static void main(String[] args) {
-        PriorityQueue<int[]> queu = new PriorityQueue<int[]>((a,b)->a[1]-b[1]);
-    }
-    void aa(Integer a){
-        a=3333;
-    }
+        LinkedList<Integer> list1 = new LinkedList();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        for(int i=0;i<100000;i++){
+            list1.addLast(i);
+            list2.add(i);
+        }
+        long startTime = System.currentTimeMillis();     //获取开始时间
+        for(int i =0;i<list1.size();i++) list1.get(i);
+        long overTime = System.currentTimeMillis();      //获取结束时间
+        System.out.println("程序运行时间为："+(overTime-startTime)+"毫秒");
+         startTime = System.currentTimeMillis();     //获取开始时间
+        for(int i =0;i<list2.size();i++) list2.get(i);
+         overTime = System.currentTimeMillis();      //获取结束时间
+        System.out.println("程序运行时间为："+(overTime-startTime)+"毫秒");
 
+
+
+    }
 }
