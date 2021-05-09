e09d559d49810ebb80d82e77f4d175e9315febef
qiujunlin <1757591067@qq.com>
2021-05-10 00:28:33 +0800

    算法4 代码

--- src/Tree/Test.java
+++ src/Tree/Test.java
@@ -1,9 +1,6 @@
 package Tree;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
@@ -15,5 +12,27 @@ public class Test {
         queue2.removeLast();
         Collections.copy(queue2,queue);
         System.out.println(queue2);
+    }
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+          List<Integer> list1 = new ArrayList<>();
+          List<Integer> list2 = new ArrayList<>();
+          dfs(root1,list1);
+          dfs(root2,list2);
+          if(list1.size()!=list2.size()) return  false;
+          for(int i=0;i<list1.size();i++){
+              if(list1.get(i)!=list2.get(i)) return false;
+          }
+          return  true;
+
+
+    }
+
+    private void dfs(TreeNode root1, List<Integer> list1) {
+       if(root1.right==null&&root1.left==null){
+           list1.add(root1.val);
+           return;
+       }
+       if(root1.left!=null) dfs(root1.left,list1);
+       if(root1.right!=null) dfs(root1.right,list1);
     }
 }
---
+++ src/WeeklyContest/week240.java
@@ -0,0 +1,78 @@
+package WeeklyContest;
+
+import java.util.*;
+
+/**
+ * @Classname Q240
+ * @Description:
+ * @Date 2021/5/9 15:18
+ * @Created by qiujunlin
+ */
+public class week240 {
+
+    public int maximumPopulation(int[][] logs) {
+    int hash[]= new int[100];
+    for(int log[]:logs){
+        for(int i=log[0];i<log[1];i++){
+            hash[i-1950]++;
+        }
+    }
+    int res=0;
+    for(int a:hash) res=Math.max(a,res);
+    return  res;
+    }
+    public int maxDistance(int[] nums1, int[] nums2) {
+          int i=0;
+         // int j=0;
+          int max=0;
+    for(int j=0;j<nums2.length;j++){
+        while (i<nums1.length&&nums1[i]>nums2[j]){
+            i++;
+        }
+        if(i<j){
+            max=Math.max(j-i,max);
+        }
+    }
+    return  max;
+    }
+
+    public int minDays(int[] bloomDay, int m, int k) {
+          if(m*k>bloomDay.length) return -1;
+          
+          int left =0;
+          int right= 0;
+          for(int a:bloomDay) right=Math.max(a,right);
+          while (left<right){
+              int mid=(left+right)>>1;
+              if(check(bloomDay,mid,m,k)){
+                  right=mid;
+              }else{
+                  left=mid+1;
+              }
+          }
+          return  left;
+                  
+    }
+
+    private boolean check(int[] bloomDay, int mid, int m, int k) {
+      boolean b[] = new boolean[bloomDay.length];
+      for(int i=0;i<bloomDay.length;i++){
+          if(bloomDay[i]<=mid) b[i]=true;
+      }
+      int count =0;
+      int res=0;
+      for(int i=0;i<bloomDay.length;i++){
+          if(b[i]){
+              count++;
+              if(count==k){
+                  res++;
+                  count=0;
+              }
+          }else{
+              count=0;
+          }
+      }
+      return  res>=m;
+    }
+
+}
