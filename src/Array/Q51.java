ecd81504a7b1816205414b22a73b96bbb1829bee
qiujunlin <1757591067@qq.com>
2021-05-10 19:30:59 +0800

    八皇后

--- src/Array/Q51.java
+++ src/Array/Q51.java
@@ -3,48 +3,42 @@ import   java.util.*;
 public class Q51 {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> res = new ArrayList<>();
-        boolean row[] = new boolean[n];
         boolean col[] = new boolean[n];
         char que[][] = new char[n][n];
         for(int i=0;i<n;i++) for(int j=0;j<n;j++) que[i][j]='.';
-        dfs(res,row,col,0,0,n,que);
+        dfs(res,col,0,n,que);
         return  res;
     }
 
-    private void dfs(List<List<String>> res, boolean[] row, boolean[] col, int r, int c,int n,char que[][]) {
-        if(c==n){
-            c=0;
-            r++;
-        }
-        if(r==n&&c==0){
+    private void dfs(List<List<String>> res,boolean[] col, int r,int n,char que[][]) {
+        if(r==n){
             addres(que,res,n);
         }
 
-        for(int k=0;k<=1;k++) {
-            if(k==0) {
-                dfs(res,row,col,r,c+1,n,que);
-            }
-            if (row[r]) {
-                return;
-            }
-            if(col[c]) return;
-            // 检查 45度角是否有皇后
-            for (int i = r - 1, j = c - 1; i >=0 && j >= 0; i--, j--) {
-                if (que[i][j] == 'Q') {
-                    return ;
-                }
-            }
-            // 检查 135度角是否有皇后
-            for(int i = r - 1, j = c +  1; i >= 0 && j < n; i--, j++) {
-                if (que[i][j] == 'Q') {
-                    return ;
-                }
-            }
-            row[r] =true;
+        for(int c=0;c<9;c++) {
+            boolean valid = check(que,col,n,c ,r);
+            if(!valid) continue;
             col[c] =true;
-            dfs(res,row,col,r,c+1,n,que);
-            row[r] =false;
+            que[r][c] ='Q';
+            dfs(res,col,r+1,n,que);
             col[c] =false;
+            que[r][c]='.';
+        }
+    }
+
+    private boolean check(char[][] que, boolean[] col, int n,int c,int r) {
+        if(col[c]) return false;
+        // 检查 45度角是否有皇后
+        for (int i = r - 1, j = c - 1; i >=0 && j >= 0; i--, j--) {
+            if (que[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // 检查 135度角是否有皇后
+        for(int i = r - 1, j = c +  1; i >= 0 && j < n; i--, j++) {
+            if (que[i][j] == 'Q') {
+                return false;
+            }
         }
     }
 
