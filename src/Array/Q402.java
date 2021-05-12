bc9fa5c6d9e60aa09867e52501d7f9913b217401
qiujunlin <1757591067@qq.com>
2021-05-12 15:30:50 +0800

    LCCUP

---
+++ src/Array/Q402.java
@@ -0,0 +1,38 @@
+package Array;
+
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Q402 {
+    public String removeKdigits(String num, int k) {
+        if(num.length()<=k) return  "0";
+        char ch[] = num.toCharArray();
+        Deque<Integer> deque = new LinkedList<>();
+        int count =0;
+        HashSet<Integer> se  = new HashSet<>();
+        for(int i=0;i<num.length();i++){
+            while (!deque.isEmpty()&&ch[deque.peekLast()]>ch[i]){
+                se.add(deque.pollFirst());
+                count++;
+                if (count==k) break;
+            }
+            if (count==k) break;
+            deque.addLast(i);
+        }
+        while (count<k){
+            se.add(deque.pollLast());
+            count++;
+        }
+        StringBuffer s  = new StringBuffer();
+        for(int  i=0;i<num.length();i++){
+            if(!se.contains(i)){
+                s.append(ch[i]);
+            }
+        }
+        int i=0;
+        while (i<s.length()-1&&s.charAt(i)=='0') i++;
+
+        return  s.substring(i,s.length()).toString();
+    }
+}
--- src/Array/Q84.java
+++ src/Array/Q84.java
@@ -10,9 +10,42 @@ import java.util.*;
  */
 public class Q84 {
     public int largestRectangleArea(int[] heights) {
-        Stack<Integer> stack = new Stack();
-        int amx = 0 ;
-        return 0;
+        //使用单调栈的解法
+        //思路 枚举高 寻找左右两侧 第一个小于当前高度的值  咩有的话 就是全部值
+        Deque<Integer> deque = new LinkedList<>();
+        int left[] = new int[heights.length];
+        Arrays.fill(left, -1);
+        int right[] = new int[heights.length];
+        Arrays.fill(right, heights.length);
+        //寻找左边
+        for (int i = 0; i < heights.length; i++) {
+            while (!deque.isEmpty() && heights[deque.peekLast()] > heights[i]) {
+                deque.pollLast();
+            }
+            if (!deque.isEmpty()) {
+                left[i] = deque.peekLast();
+            }
+            deque.offer(i);
+        }
+        //寻找右边
+        deque.clear();
+        for (int i = heights.length - 1; i >= 0; i--) {
+            while (!deque.isEmpty() && heights[deque.peekLast()] > heights[i]) {
+                deque.pollLast();
+            }
+            if (!deque.isEmpty()) {
+                right[i] = deque.peekLast();
+            }
+            deque.offer(i);
+        }
+        int max = 0;
+        for (int i = 0; i < heights.length; i++) {
+            int len = right[i] - left[i] - 1;
+            max = Math.max(len * heights[i], max);
+        }
+        return max;
+
 
     }
 }
+
