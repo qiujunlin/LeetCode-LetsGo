fdec6f54b48f06f6ab9773eae959934c0b2531cd
qiujunlin <1757591067@qq.com>
2021-06-27 00:11:57 +0800

    排序

--- src/Array/Q1723.java
+++ src/Array/Q1723.java
@@ -44,4 +44,37 @@ public class Q1723 {
          }
          return  false;
     }
+    int ans =Integer.MAX_VALUE;
+    int k ;
+    public int minimumTimeRequired2(int[] jobs, int _k) {
+        k = _k;
+        int assign[] = new int[k];
+        dfs2(jobs,assign,0,0,0);
+        return  ans;
+    }
+
+    /**
+     *
+     * @param jobs
+     * @param assign
+     * @param j  jobs
+     * @param i   assign
+     */
+    private void dfs2(int[] jobs, int[] assign, int j, int i,int max) {
+        if(ans<max) return;
+        if(j==jobs.length){
+            ans = max;
+            return;
+        }
+          if(i<k){
+              assign[j] = jobs[i];
+              dfs2(jobs,assign,j+1,i+1,Math.max(assign[j],max));
+              assign[j] =0;
+          }
+        for (int l = 0; l < i; l++) {
+            assign[l]+=jobs[j];
+            dfs2(jobs,assign,j+1,i,Math.max(assign[j],max));
+            assign[l] -= jobs[j];
+        }
+    }
 }
