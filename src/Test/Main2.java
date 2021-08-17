7debc4443265ee3f7635451dec3caab420256539
qiujunlin <1757591067@qq.com>
2021-08-17 22:32:14 +0800

    zhousai

--- src/Test/Main2.java
+++ src/Test/Main2.java
@@ -6,17 +6,18 @@ FE65CDBA
 4268084666
  */
 import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
 
-        Pattern pattern = Pattern.compile("\"^[0-9a-zA-Z_]+$\"");
-        System.out.println(pattern.matcher("second".substring(1)).matches());
-
+       String host  =  InetAddress.getLocalHost().getHostAddress();
+        System.out.println(host);
     }
 
 }
