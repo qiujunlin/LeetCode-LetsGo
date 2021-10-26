1448aa5588a9c7261dfb454c39c18fba2dac6df6
qiujunlin <1757591067@qq.com>
2021-10-26 16:37:08 +0800

    每日一题

--- src/Test/Main2.java
+++ src/Test/Main2.java
@@ -9,6 +9,12 @@ import java.util.regex.Pattern;
 
 public class Main2 {
     public static void main(String[] args) {
+       String s  ="汉";
+       byte[] b = s.getBytes();
+       for (int i = 0; i <b.length ; i++) {
+            System.out.println(Integer.toHexString(Integer.valueOf(b[i])));
+        }
+        System.out.println(b.length);
 
 
     }
