acea5b2b5df75c83ce33a669967d57db8b5e4da0
qiujunlin <1757591067@qq.com>
2021-11-03 15:20:47 +0800

    每日一题

--- src/Test/Main2.java
+++ src/Test/Main2.java
@@ -10,35 +10,12 @@ import java.util.regex.Pattern;
 
 public class Main2 {
     public static void main(String[] args) {
-        new Main2().getTypeString();
-
+         StringBuilder s = new StringBuilder("abc");
+         s.setCharAt(0,'d');
+        System.out.println(s);
 
     }
-    public enum TypeEnum {
-        ZERO,
-        ONE
-    }
 
-    private TypeEnum type;
-
-    public TypeEnum getType() {
-        return type;
-    }
-
-    public void setType(TypeEnum type) {
-        this.type = type;
-    }
-
-    public String getTypeString() {
-        switch (type) {
-            case ZERO:
-                return "ZERO";
-            case ONE:
-                return "ONE";
-            default:
-                return "OTHER";
-        }
-    }
 
 
 }
---
+++ src/leetcode/Q407.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import java.util.PriorityQueue;
+
+public class Q407 {
+    public int trapRainWater(int[][] heightMap) {
+        //
+        int m =  heightMap.length;
+        int n = heightMap[0].length;
+        PriorityQueue<int[]> q = new PriorityQueue();
+        boolean vis[][] = new boolean[m][n];
+        for (int i = 0; i <m ; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i==0||j==0||i==m-1||j==n-1){
+                    vis[i][j]=true;
+                    q.offer(new int[]{i,j,heightMap[i][j]});
+                }
+            }
+        }
+        int res =0;
+        int direct[][] = new int[][]{{0,-1},{0,1},{-1,0},{1,0}};
+        while (!q.isEmpty()){
+
+            int nums[] = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int newx = nums[0]+direct[i][0];
+                int newy = nums[1]+direct[i][1];
+                if(newx>=0&&newy>=0&&newx<m&&newy<n&&!vis[newx][newx]){
+                     if(heightMap[newx][newy]<nums[2]){
+                         res+=heightMap[newx][newy]-nums[2];
+                     }
+                     q.offer(new int[]{newx,newy,Math.max(heightMap[newx][newy],nums[2])});
+                     vis[newx][newy] =true;
+                }
+            }
+        }
+        return  res;
+
+
+    }
+}
