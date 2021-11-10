58a6e51bf1af0a6b822aede772830468cbbb8812
qiujunlin <1757591067@qq.com>
2021-11-10 21:03:27 +0800

    周赛

--- src/Test/Main2.java
+++ src/Test/Main2.java
@@ -9,10 +9,18 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 public class Main2 {
-    public static void main(String[] args) {
-       int a = 0x00000001;
-       System.out.println(a);
+    public static void main(String[] args) throws   Exception {
+        List<String >  list =  new ArrayList<>();
+
+      // List<String> list =   new ArrayList<>();
+       list.getClass().getMethod("add",Object.class).invoke(list,1);
     }
+ public void test(Collection<?> collection){
+
+ }
+public <T> void test2(Collection<T> collection){
+
+}
 
 
 
--- src/Test/Main3.java
+++ src/Test/Main3.java
@@ -1,81 +1,32 @@
 package Test;
+import com.sun.org.apache.xerces.internal.xs.StringList;
 import edu.princeton.cs.algs4.Stack;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import  java.util.*;
 public class Main3 {
-    class Node{
-        Node nodes[] ;
-        boolean end ;
-        public Node(){
-            this.nodes = new Node[26];
-            this.end= false;
+    public static void main(String[] args) throws  Exception{
+        Base<String>  ba = new Base<String>();
+        for(Field field :ba.getClass().getFields()){
+            Type gtype = field.getGenericType();
+            Type type[] = ((ParameterizedType) gtype).getActualTypeArguments();
+            System.out.println(type[0].getTypeName());
+            System.out.println("定义的泛型："+field.getGenericType()+" 实际的类型： " + field.getType());
         }
     }
-    String res ="";
-    public String longestWord(String[] words) {
-        //字典树  1. 构建树  2 dfs
-        Node n = new Node();
-        for(String s :  words){
-            Node  temp = n;
-            for(int i=0;i<s.length();i++){
-                char c = s.charAt(i);
-                if(temp.nodes[c-'a']==null){
-                    temp.nodes[c-'a'] =new Node();
-                }
-                temp = temp.nodes[c-'a'];
-            }
-            temp.end = true;
-        }
-
-        return "";
-    }
-    void dfs(Node n,String s){
-        if(res.length()<s.length()){
-            res =s;
-        }
-        for(int i =0;i<26;i++){
-            if(n.nodes[i]!=null){
-                if(n.end){
-                    dfs(n.nodes[i],s+String.valueOf((char)(i+'a')));
-                }
-            }
-        }
-    }
-     public static void main(String[] args) {
-        int nums[] ={1,2,3,4};
-        int nums2[] ={4,3,2,4,3};
-        new Main3().fun(5,3,nums2);
-
-    }
-    public  void fun(int n,int m,int nums[]){
-        Stack<Integer> stack =  new Stack<>();
-        for (int i = 0; i < nums.length; i++) {
-            while (!stack.isEmpty()&&nums[stack.peek()]<nums[i]){
-                  int a  =stack.pop();
-                  int c = -1;
-                  if(!stack.isEmpty())c = stack.peek();
-                  int count =0;
-                  if(c!=-1) {
-                      if(i==n-1&nums[c]>nums[i]) {
-                          count = (i-a)*(nums[i]-nums[a]);
-                      }else
-                      count = (i - a) * (nums[c] - nums[a]);
-                      if(m-count<=0){
-                          System.out.println(m%(i-a)==0?c+2:i-m%(i-a)+1);
-                          return;
-                      }else m-=count;
-                  }else{
-                      count =  (nums[i]-nums[a])*i;
-                      if(m-count<=0){
-                          System.out.println(m%a==0?1:i-m%a+1);
-                          return;
-                      }else m-=count;
-                }
-            }
-            stack.push(i);
-        }
-        System.out.println(n+1);
-    }
-
-
 }
+class Base<T >{
+    public T  t;
+    public HashMap<T,T> map;
+    public  List<T> list;
+
+    public  void test(T t){
+        System.out.println(t);
+    }
+}
+
+
+
