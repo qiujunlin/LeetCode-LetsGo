b65b797ab7b1bf088e9705a5ef0fede871a313a6
qiujunlin <1757591067@qq.com>
2022-07-05 23:54:39 +0800

    LEETCODE

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -12,64 +12,7 @@ public class Main {
      * @author liuhuifang
      * @date 2022/5/26 11:31
      */
-    class LFUCache {
-        class Node{
-            int key;int val;int age;
-            public Node(int key,int val){this.key =key;this.val =val;}
 
-        }
-        HashMap<Integer,Node> map =new HashMap();
-        HashMap<Integer,LinkedList<Node>> map2 =new HashMap();
-        int cap ;
-        int  minlevel = 0;
-        public LFUCache(int capacity) {
-            this.cap =capacity;
-        }
-
-        public int get(int key) {
-            if(map.containsKey(key)) {
-                Node n = map.get(key);
-                LinkedList l = map2.get(n.age);
-                l.remove(n);
-                n.age++;
-                map2.computeIfAbsent(n.age,(k)->new LinkedList()).addLast(n);
-                if(minlevel==n.age-1&&l.size()==0) minlevel = n.age;
-
-            }else{
-                return -1;
-            }
-
-        }
-
-        public void put(int key, int value) {
-            if(map.containsKey(key)){
-                Node  n = map.get(key);
-                LinkedList l = map2.get(n.age);
-                l.remove(n);
-                n.age++;
-                n.val =value;
-                map2.compteIfAbsent(n.age,(k)->new LinkedList()).addLast(n);
-                if(minlevel==n.age-1&&l.size()==0) minlevel = n.age;
-            }else{
-                if(map.size()==cap){
-                    Node  node = map2.get(minlevel).removeFirst();
-                    map.remove(node.key);
-                }
-                Node n =new Node(key,value);
-                map2.compteIfAbsent(n.age,(k)->new LinkedList()).addLast(n);
-                minlevel = 0;
-            }
-
-
-        }
-    }
-
-    /**
-     * Your LFUCache object will be instantiated and called as such:
-     * LFUCache obj = new LFUCache(capacity);
-     * int param_1 = obj.get(key);
-     * obj.put(key,value);
-     */
     public static void main(String[] args) throws InterruptedException {
         Semaphore semaphore =new Semaphore(2);
        // System.out.println( semaphore.);
