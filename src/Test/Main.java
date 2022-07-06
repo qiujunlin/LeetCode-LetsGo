ee985c2669c1ae70407f87717694dd2368a6f555
qiujunlin <1757591067@qq.com>
2022-07-06 23:52:37 +0800

    LEETCODE

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -6,11 +6,23 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
+    class Node{
+        int a;
+        String b;
+
+        @Override
+        public boolean equals(Object obj) {
+            Node a = (Node)obj;
+            String.
+            return  this.a == a.a && this.b.equals(a.b);
+        }
+    }
 
         //初始化ReentrantLock
     /**
      * @author liuhuifang
      * @date 2022/5/26 11:31
+     *
      */
 
     public static void main(String[] args) throws InterruptedException {
