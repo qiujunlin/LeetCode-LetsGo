6a917637748ab4dd8b2066ed20e90884c1796bcf
qiujunlin <1757591067@qq.com>
2022-08-20 13:35:14 +0800

    笔试

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 
 import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public  class  Main {
+    /**
+
+     *
+     * @param args
+     */
+
     public static void main(String[] args) {
 
+        ArrayBlockingQueue<Integer> queue =new ArrayBlockingQueue(1);
+
+
     }
-
-
 }
