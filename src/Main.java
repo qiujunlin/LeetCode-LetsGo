c988fb3a2b2e88abb6dedd0bb04cdd138255e417
qiujunlin <1757591067@qq.com>
2022-08-26 22:12:52 +0800

    笔试

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,40 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.*;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
+    public Map<Integer, String> map = new HashMap<>();
+
     public static void main(String[] args) {
-        Scanner scn =new Scanner(System.in);
-        int n =scn.nextInt();
-        int m =scn.nextInt();
-        StringBuilder s =new StringBuilder();
-        for (int i = 0; i <n ; i++) {
-            s.append("R");
-        }
-        s.append("P");
-        for (int i = 0; i <m ; i++) {
-            s.append("R");
-        }
-        System.out.println(s);
-
-
-
+    ArrayList<Integer> l =new ArrayList<>();
+    l.iterator()
 
 
 
     }
+
+     class SampleClass {
+        private String sampleField;
+        private List<Integer> ids;
+        public String getSampleField() {
+            return sampleField;
+        }
+        public void setSampleField(String sampleField) {
+            this.sampleField = sampleField;
+        }
+        public List<Integer> getIds() {
+            return ids;
+        }
+        public void setIds(List<Integer> ids) {
+            this.ids = ids;
+        }
+    }
+
 }
+
